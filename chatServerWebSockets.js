e617bd51240a81fac3b96b54d4dacb7f1a8d2bfe
TerryMooreII <terry.moore.ii@gmail.com>
2013-07-16 21:47:24 -0400

    Combined

---
+++ chatServerWebSockets.js
@@ -0,0 +1,71 @@
+var connect = require('connect');
+
+connect.createServer(
+    connect.static(__dirname)
+).listen(8080);
+console.log('Listening on port 8080');
+
+
+var io = require('socket.io').listen(8089,{origins: '*:*'});
+
+io.sockets.on('connection', function(socket){
+    var userList = [];
+
+    socket.on('newUser', function(data){
+        
+        socket.username = data.user;
+        
+        socket.to(socket.room).broadcast.emit('message', {
+            user: "Node",
+            timestamp: new Date(),
+            message: data.user + " has joined the room",
+            messageType: 'system'
+        });
+        getUserList();      
+    });
+
+    socket.on('newRoom', function(room){
+        socket.leave(socket.room);
+        socket.room = room;
+        socket.join(room);
+    });
+
+
+    socket.on('message', function(data){
+        var message = {
+            user: data.user,
+            timestamp: new Date(),
+            message: data.message,
+            messageType: 'user'
+        };
+        socket.in(socket.room).emit('message', message) 
+        socket.in(socket.room).broadcast.emit('message', message);
+    });
+
+    socket.on('leaving', function(data){
+        socket.leave(socket.room)
+        getUserList();  
+        socket.to(socket.room).broadcast.emit('message', {
+            user: "Node",
+            timestamp: new Date(),
+            message: data.user + " has left the room",
+            messageType: 'system'
+        });
+    });
+
+    var getUserList = function(){
+        userList = [];
+        for (var i=0; i < io.sockets.clients(socket.room).length; i++){
+            console.log(io.sockets.clients(socket.room)[i].username);
+            userList.push(io.sockets.clients(socket.room)[i].username);
+        }
+    
+        socket.in(socket.room).emit('userList', {
+            users: userList
+        });
+        socket.to(socket.room).broadcast.emit('userList', {
+            users: userList
+        });
+    }
+
+});
---
+++ js/main.js
@@ -0,0 +1,109 @@
+
+(function(){
+    var handle = 'anonymous';
+    var socket = null;
+    var room = 'main';
+   
+    $('#newRoom').val(window.location.hash.substring(1))
+
+    $('.modal').modal();
+    $('.modal a').on('click', function(){
+        var h = $('#handle').val();
+        if ( h != ''){
+            handle = h;
+            $('.modal').modal('hide'); 
+            establishConnection(handle);   
+        }
+    });
+
+    $('#newRoomBtn').on('click', function(e){
+        e.preventDefault;
+        var data = {
+            user: handle
+        };
+        socket.emit('leaving', data);
+        socket.disconnect();    
+        establishConnection();
+        window.location.hash = $(this).val();
+    })
+
+    var establishConnection = function(){
+        socket = null;
+        options = [];
+        options['force new connection'] = true;
+        
+        socket = io.connect('http://localhost:8089', options);
+        
+        room = $('#newRoom').val();
+        socket.emit('newRoom', room);
+
+        var data = {
+            user: handle
+        };
+        
+        socket.emit('newUser', data); 
+
+        loadChatEvents();
+        $('#messages').empty();
+    }
+
+    var loadChatEvents = function(){
+            
+        // Log messages from the server
+        socket.on('message', function (data) {
+            var message = '';
+            if (data.messageType === 'system'){
+                message = "<li style='padding:10px 0px'><span class='alert alert-info'>" + data.message + "</span></li>";
+            }else{
+                message = "<li style='padding:5px'><strong>"+data.user+"</strong>: " + sanitze(data.message) + "</li>";
+            }
+            $('#messages').append(message)
+            $('#messages').scrollTop($('#messages')[0].scrollHeight);
+        });
+        socket.on('userList', function (data) {
+            $('#whos-online').empty();
+            $.each(data.users, function(k, handle){
+                $('#whos-online').append('<li><i class="icon-user"></i><span style="padding:5px">'+handle+'</span></li>')    
+            });
+            
+        });
+
+        $('#message').on('keyup', function(e){
+            if (e.keyCode === 13)
+                $('#send').trigger('click');
+        });
+
+        $('#send').on('click', function(e){
+            e.preventDefault();
+            var text = $('#message').val();
+
+            if (text === '')
+                return false;
+
+            var data = {
+                user: handle,
+                message: text
+            };
+
+            socket.emit('message', data);
+            $('#message').val('');
+        });
+
+        var sanitze = function(message){
+            return message.replace(/</g, '&lt;')
+                          .replace(/>/g, '&gt;')
+                          .replace(/\n/g, '<br>')
+                          .replace(/\n\r/g, '<br>')
+                          .replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;')
+                          .replace(/ /g, '&nbsp')
+        }
+
+        window.onbeforeunload = function(e){
+            var data = {
+                user: handle
+            };
+            socket.emit('leaving', data);
+        }
+    }
+
+})();
